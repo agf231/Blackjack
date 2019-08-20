659b4ed723f3bdfa16761baa1ac846f1a42dc50d
agf231 <50413884+agf231@users.noreply.github.com>
2019-08-20 15:30:41 -0500

    Updated blackjack.py
    Added preliminary versions of functions for scoring, player turn, dealer turn, and more

--- blackjack.py
+++ blackjack.py
@@ -15,8 +15,10 @@ Created on Thu Jun 27 12:54:16 2019
 #allow multiple decks
 import random
 
-values = [ "Ace", "2", "3", "4", "5", "6", "7","8", "9", "10", "Jack", "Queen", "King" ]
+
+values = [ "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King" ]
 suits = [ "Clubs", "Diamonds", "Hearts", "Spades" ]
+
 
 class Card:
     def __init__(self, number, suit):
@@ -24,7 +26,8 @@ class Card:
         self.suit = suit
 
     def show(self):
-        print("{} of {}".format(self.suit, self.number))
+        print("{} of {}".format(self.number, self.suit))
+
 
 class Deck:
     def __init__(self):
@@ -32,9 +35,9 @@ class Deck:
         self.build()
 
     def build(self):
-        for i in suits:
+        for s in suits:
             for v in values:
-                self.cards.append(Card(i, v))
+                self.cards.append(Card(v, s))
 
     def show(self):
         for c in self.cards:
@@ -47,6 +50,7 @@ class Deck:
 
     def drawCard(self):
         return self.cards.pop()
+
 
 class Player:
     def __init__(self, name):
@@ -61,17 +65,68 @@ class Player:
         for card in self.hand:
             card.show()
 
+    def score(self):
+        total = 0
+        for i in self.hand:
+            if type(i.number) == int:
+                total += i.number
+            if i.number == "Ace" and 11 + total < 22:
+                total += 11
+            elif i.number == "Ace" and 11 + total > 22:
+                total += 1
+            elif type(i.number) == str:
+                total += 10
+        return(total)
+     
+
+def dealCards():
+    player.draw(deck)
+    dealer.draw(deck)
+    print("Dealer's showing:")
+    dealer.showHand()
+    player.draw(deck)
+    dealer.draw(deck)
+        
+
+def playerTurn():
+    playerTotal = player.score()
+    command = input("Type 'H' to hit or 'S' to stay: ".lower())
+   
+    while command == 'h' and playerTotal < 22:      
+        player.draw(deck)
+        playerTotal = player.score()
+        print(playerTotal)
+       
+        if playerTotal < 22:       
+            command = input("Type 'H' to hit or 'S' to stay: ".lower())
+    
+    player.showHand()
+    print('Final score is {}'.format(playerTotal))
+
+
+def dealerTurn():
+    dealerTotal = dealer.score()
+    
+    while dealerTotal < 17:
+        dealer.draw(deck)
+        dealerTotal = dealer.score()
+    
+    print("Dealer's final score is {}".format(dealerTotal))
+    dealer.showHand()
+
 
 deck = Deck()
 deck.shuffle()
 
-card = deck.drawCard()
-card.show()
-print("----------------------------------------------------")
-deck.show()
-print("----------------------------------------------------")
-tester = Player("Tester")
-tester.draw(deck)
-tester.showHand()
-print("----------------------------------------------------")
-deck.show()
+player = Player("User")
+dealer = Player("Dealer")
+          
+dealCards()
+
+print("\nPlayer has:")
+print(player.showHand())
+print(player.score())
+
+playerTurn()
+dealerTurn()
+    
